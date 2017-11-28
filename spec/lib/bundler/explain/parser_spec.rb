18498d4b30588807e8dea54200847a7aa0d8fddb
Dmitriy <vkosmos26@yandex.ru>
2017-11-28 14:05:14 +0800

    First spec for parser

--- spec/lib/bundler/explain/parser_spec.rb
+++ spec/lib/bundler/explain/parser_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe Bundler::Explain::Parser do
   describe '#call' do
-    it 'process file to hash'
+    it 'process file to hash' do
+      file_path = 'examples/case1/Gemfile.lock'
+      result = Bundler::Explain::Parser.new(file_path).call
+      expect(result.dig('GEM', 'specs').first.keys).to match_array ['actionpack (= 5.1.4)', 'nio4r (~> 2.0)', 'websocket-driver (~> 0.6.1)']
+    end
   end
 end
